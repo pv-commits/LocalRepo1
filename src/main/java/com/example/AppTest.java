0ebb7d332674c708c7eb211a517e32dd28499c7f
pv-commits <134913775+pv-commits@users.noreply.github.com>
2024-08-20 23:43:48 +0530

    Update AppTest.java

--- src/main/java/com/example/AppTest.java
+++ src/main/java/com/example/AppTest.java
@@ -1,11 +1,1 @@
-package com.example;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-
-public class AppTest {
-    @Test
-    public void testApp() {
-        assertTrue(true);  // Simple test to always pass
-    }
-}
